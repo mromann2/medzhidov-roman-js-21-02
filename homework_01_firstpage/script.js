af1c4f592266c5df6b26f443c47e49bd34545098
mromann2 <mromann@inbox.ru>
2021-10-02 17:14:12 +0300

    fix grid-column-gap

--- homework_01_firstpage/script.js
+++ homework_01_firstpage/script.js
@@ -1,7 +1,7 @@
-let page = document.querySelector('.main');
+let main = document.querySelector('.main');
 let themeButton = document.querySelector('.header__theme-button');
 
 
 themeButton.onclick = function() {
-page.classList.toggle('main_theme_dark');
+main.classList.toggle('main_theme_dark');
 };
