e4fc40c3a7a45b7a15e412cd3db09e971a7acf51
mromann2 <mromann@inbox.ru>
2021-10-04 21:57:11 +0300

    card animation

--- homework_01_firstpage/script.js
+++ homework_01_firstpage/script.js
@@ -1,9 +1,11 @@
 let main = document.querySelector('.main');
 let header = document.querySelector('.header');
 let themeButton = document.querySelector('.header__theme-button');
+let fishBuys = document.querySelectorAll('.fish-buy');
 
 
 themeButton.onclick = function() {
 main.classList.toggle('main_theme_dark');
 header.classList.toggle('header_theme_dark');
+fishBuys.forEach(el => el.classList.toggle('fish-buy_theme_dark'))
 };
