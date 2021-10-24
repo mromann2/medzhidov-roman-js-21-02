237ae91f27c677f00dfa25e16d8b21ee1b4d67c0
mromann2 <mromann@inbox.ru>
2021-10-24 18:30:26 +0300

    fin

---
+++ homework/10_lesson/move-funcs.mjs
@@ -0,0 +1,29 @@
+let loaderBox = document.querySelector('.loader-box')
+let pos = Number.parseInt(getComputedStyle(loaderBox).left)
+let interval;
+
+
+function moveRight() {
+    return interval = requestAnimationFrame(() => {
+        if (pos < Number.parseInt(getComputedStyle(loaderBox.parentElement).width) - Number.parseInt(getComputedStyle(loaderBox).width)) {
+            pos+=4;
+            loaderBox.style.left = `${pos}px`
+            interval = requestAnimationFrame(moveRight)
+        } else {
+            interval = requestAnimationFrame(moveLeft)
+        }
+    })
+}
+function moveLeft() {
+    return interval = requestAnimationFrame(() => {
+        if (pos >= 0) {
+            pos-=4;
+            loaderBox.style.left = `${pos}px`
+            interval =  requestAnimationFrame(moveLeft)
+        } else {
+            interval = requestAnimationFrame(moveRight)
+        }
+    })
+}
+
+export {moveRight, interval};
