c041ef7be2046daa2b5ca411d27b8d750109be90
mromann2 <mromann@inbox.ru>
2021-11-22 09:01:21 +0300

    add empty line

---
+++ homework/22_lesson/redux/src/actions/ListActions.ts
@@ -0,0 +1,8 @@
+import { ADD_RECORD } from '../constants/actions/list';
+import { ListActionType } from '../types/actions';
+
+export const addRecordAction = (newRecord: string): ListActionType => ({
+    type: ADD_RECORD,
+    newRecord,
+});
+
